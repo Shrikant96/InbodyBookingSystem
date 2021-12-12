bddfa6f0a538baa17f41ef21f188604709d17af6
Shrikant96 <shrikantgith@gmail.com>
2021-12-12 15:21:56 +0530

    changed settings and temp

--- bookingsystem/settings.py
+++ bookingsystem/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
-STATIC = os.path.join(BASE_DIR,"static")
+TEMPLATE_DIR = BASE_DIR/"templates"
+STATIC = BASE_DIR/"static"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -120,10 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-            
-]
+STATICFILES_DIRS = [STATIC]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
